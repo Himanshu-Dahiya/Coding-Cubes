f1a72041bda3f90c540583c9be354c80012cc4cd
Himanshu Dahiya <himanshudahiya215@gmail.com>
2020-02-19 23:01:46 +0530

    animating text in slider section

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ jQuery(document).ready(function(){
     "use strict"
       $('.slider').ripples({
       dropRadius: 13,
-      perturbance: 0.01
-
+      perturbance: 0.01,
 }); 
+    
+     $('.text').typed({
+        strings: ['<strong> I LOVE </strong> <strong class="primary"> CODINGS. </strong>','<strong> AND TO </strong> <strong class="primary"> SHARE!!  </strong>'],
+         typespeed: 0,
+         loop:true
+     });
 });
 
 
